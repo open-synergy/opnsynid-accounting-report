82b94951439ba0981749dccd4959fe373cde48e9
mike <mike@mike.(none)>
2015-12-11 15:15:41 +0700

    Adding Module report_trial_balance

---
+++ report_trial_balance/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'KOI Accounting - Accounting Report Based Using Aeroo Report',
+    'version': '1.1',
+    'author': 'Kawula OpenERP Indonesia; Andhitia Rama & Michael Viriyananda',
+    'category': 'Accounting',
+    'summary': 'Accounting Report Based Using Aeroo Report for KOI Accounting Application',
+    'description': """
+
+    """,
+    'website': 'https://opensynergy-indonesia.com',
+    'images': [],
+    'depends': [
+        'account_accountant',
+        # 'report_aeroo_ooo'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'report/report_trial_balance.xml',
+        'wizard/wizard_trial_balance.xml',
+        'menu_Accounting.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'AGPL-3'
+}
