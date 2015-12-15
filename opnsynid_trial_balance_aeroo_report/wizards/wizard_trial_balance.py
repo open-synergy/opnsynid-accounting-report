42e0b7a3a213ddd54e80b173f42d3779f3e51174
mike <mike@mike.(none)>
2015-12-15 13:54:04 +0700

    Fixing Issues08

--- opnsynid_trial_balance_aeroo_report/wizards/wizard_trial_balance.py
+++ opnsynid_trial_balance_aeroo_report/wizards/wizard_trial_balance.py
@@ -18,81 +18,72 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-
-from openerp.osv import osv, fields
+from openerp import models, fields, api, osv
 from openerp.tools.translate import _
 
 
-class wizard_report_trial_balance(osv.osv_memory):
+class wizard_report_trial_balance(models.Model):
     _name = 'account.wizard_report_trial_balance'
     _description = 'Wizard Report Trial Balance'
 
-    def default_company_id(self, cr, uid, context=None):
-        obj_user = self.pool.get('res.users')
+    @api.model
+    def _default_company_id(self):
+        return self.env.user.company_id.id
 
-        user = obj_user.browse(cr, uid, [uid])[0]
-
-        return user.company_id and user.company_id.id or False
-
-    def default_fiscalyear_id(self, cr, uid, context=None):
-        obj_fiscalyear = self.pool.get('account.fiscalyear')
-
-        fiscalyear_id = obj_fiscalyear.find(cr, uid)
-
+    @api.model
+    def _default_fiscalyear_id(self):
+        fiscalyear_id = self.env['account.fiscalyear'].find()
         return fiscalyear_id or False
 
-    def default_period_id(self, cr, uid, context=None):
-        obj_period = self.pool.get('account.period')
-
-        period_ids = obj_period.find(cr, uid)
-
+    @api.model
+    def _default_period_id(self):
+        period_ids = self.env['account.period'].find()
         return period_ids and period_ids[0] or False
 
-    def default_state(self, cr, uid, context=None):
+    @api.model
+    def default_state(self):
         return 'posted'
 
-    def default_output(self, cr, uid, context=None):
+    @api.model
+    def default_output(self):
         return 'ods'
 
-    _columns = {
-        'company_id': fields.many2one(
-            string='Company',
-            obj='res.company',
-            required=True),
-        'fiscalyear_id': fields.many2one(
-            string='Fiscal Year',
-            obj='account.fiscalyear',
-            required=True),
-        'period_id': fields.many2one(
-            string='Period',
-            obj='account.period',
-            required=True),
-        'output_format': fields.selection(
-            string='Output Format',
-            required=True,
-            selection=[
-                ('pdf', 'PDF'),
-                ('xls', 'XLS'),
-                ('ods', 'ODS')
-            ]),
-        'state': fields.selection(
-            string='State',
-            selection=[
-                ('all', 'All'),
-                ('draft', 'Draft'),
-                ('posted', 'Posted')
+    company_id = fields.Many2one(
+        string='Company',
+        comodel_name='res.company',
+        required=True,
+        default=_default_company_id,
+        )
+    fiscalyear_id = fields.Many2one(
+        string='Fiscal Year',
+        comodel_name='account.fiscalyear',
+        required=True,
+        default=_default_fiscalyear_id,
+        )
+    period_id = fields.Many2one(
+        string='Period',
+        comodel_name='account.period',
+        required=True,
+        default=_default_period_id,
+        )
+    output_format = fields.Selection(
+        string='Output Format',
+        required=True,
+        default='ods',
+        selection=[
+            ('xls', 'XLS'),
+            ('ods', 'ODS')
+        ])
+    state = fields.Selection(
+        string='State',
+        selection=[
+            ('all', 'All'),
+            ('draft', 'Draft'),
+            ('posted', 'Posted')
             ],
-            required=True),
-    }
-
-    _defaults = {
-        'company_id': default_company_id,
-        'fiscalyear_id': default_fiscalyear_id,
-        'period_id': default_period_id,
-        'output_format': default_output,
-        'state': default_state
-    }
+        required=True,
+        default='posted',
+        )
 
     def button_print_report(self, cr, uid, ids, data, context=None):
         datas = {}
