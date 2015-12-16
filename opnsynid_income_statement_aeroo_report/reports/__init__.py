bf42c5b15300205de2943817778fdc1e8cb5de18
Andhitia Rama <andhitia.r@gmail.com>
2015-12-16 23:29:44 +0700

    Fix import module error

--- opnsynid_income_statement_aeroo_report/reports/__init__.py
+++ opnsynid_income_statement_aeroo_report/reports/__init__.py
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import report_balance_sheet
+from . import report_income_statement
