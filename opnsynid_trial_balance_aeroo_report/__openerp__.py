fc0c9d576ecf45f0b011bb9a00b8ecb479d06015
Andhitia Rama <andhitia.r@gmail.com>
2016-03-19 03:17:15 +0700

    Use simple header. Fix manifest file

--- opnsynid_trial_balance_aeroo_report/__openerp__.py
+++ opnsynid_trial_balance_aeroo_report/__openerp__.py
@@ -1,34 +1,14 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'Trial Balance Report',
     'version': '8.0.1.0.0',
-    'author': 'OpenSynergy Indonesia; Michael Viriyananda',
+    'author': 'OpenSynergy Indonesia',
     'category': 'Accounting',
     'summary': 'Report Trial Balance',
-    'description': """
-
-    """,
     'website': 'https://opensynergy-indonesia.com',
-    'images': [],
     'depends': [
         'account_accountant',
         'report_aeroo'
