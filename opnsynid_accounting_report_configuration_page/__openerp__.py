9c998bd9a19787d0ebec9e10d920a1996831580b
Andhitia Rama <andhitia.r@gmail.com>
2016-03-19 03:45:09 +0700

    Add README. Use simple header. Fix manifest

--- opnsynid_accounting_report_configuration_page/__openerp__.py
+++ opnsynid_accounting_report_configuration_page/__openerp__.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c) 2009 CamptoCamp. All rights reserved.
-#    @author Nicolas Bessi
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Accounting report configuration page",
     "version": "8.0.1.0.0",
-    "author": "Andhitia Rama,OpenSynergy Indonesia",
+    "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "license": "AGPL-3",
     "category": "Financial Management/Configuration",
@@ -31,7 +15,6 @@
     "data": [
         "views/res_company_view.xml",
     ],
-    "demo": [],
     "active": False,
     'installable': True
 }
