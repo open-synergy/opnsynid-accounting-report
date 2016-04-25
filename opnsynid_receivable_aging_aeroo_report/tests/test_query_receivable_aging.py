178a6ed98de8806407beae7888801f88a8c3abf1
mike <viriyananda.michael@gmail.com>
2016-04-25 07:32:36 +0700

    [ADD] Add new unittest for payments

--- opnsynid_receivable_aging_aeroo_report/tests/test_query_receivable_aging.py
+++ opnsynid_receivable_aging_aeroo_report/tests/test_query_receivable_aging.py
@@ -13,8 +13,12 @@ class TestQueryReceivableAging(TransactionCase):
         self.obj_query = self.env['account.query_receivable_aging']
         self.obj_invoice = self.env['account.invoice']
         self.obj_move = self.env['account.move']
+        self.obj_move_line = self.env['account.move.line']
+        self.obj_bank_stmt = self.env['account.bank.statement']
+        self.obj_bank_stmt_line = self.registry('account.bank.statement.line')
 
         self.journal_1 = self.env.ref('account.sales_journal')
+        self.bank_journal_1 = self.env.ref('account.bank_journal')
         self.partner_1 = self.env.ref('base.res_partner_2')
         self.currency_1 = self.env.ref('base.EUR')
         self.company = self.env.user.company_id.id
@@ -24,6 +28,14 @@ class TestQueryReceivableAging(TransactionCase):
 
         self.period = self.env.ref('account.period_2')
         self.date_now = self.period.date_start
+
+        self.date_payment = date.fromordinal(
+            datetime.strptime(
+                self.date_now, '%Y-%m-%d').toordinal() + 3)
+
+        self.date_payment_2 = date.fromordinal(
+            datetime.strptime(
+                self.date_now, '%Y-%m-%d').toordinal() + 40)
 
         self.date_due = date.fromordinal(
             datetime.strptime(
@@ -57,7 +69,46 @@ class TestQueryReceivableAging(TransactionCase):
         }
         return data
 
-    def test_query_receivable_aging(self):
+    def _prepare_bank_statement(self):
+        data = {
+            'journal_id': self.bank_journal_1.id,
+            'date': self.date_payment
+        }
+        return data
+
+    def _prepare_bank_statement_line(self, bank_stmt_id):
+        data = {
+            'name': 'First Payment',
+            'statement_id': bank_stmt_id,
+            'partner_id': self.partner_1.id,
+            'amount': 150,
+            'currency_id': self.currency_1.id,
+            'date': self.date_payment
+        }
+        return data
+
+    def _prepare_bank_statement_2(self):
+        data = {
+            'journal_id': self.bank_journal_1.id,
+            'date': self.date_payment_2
+        }
+        return data
+
+    def _prepare_bank_statement_line_2(self, bank_stmt_id):
+        data = {
+            'name': 'Second Payment',
+            'statement_id': bank_stmt_id,
+            'partner_id': self.partner_1.id,
+            'amount': 70,
+            'currency_id': self.currency_1.id,
+            'date': self.date_payment_2
+        }
+        return data
+
+    def test_query_receivable_aging_1(self):
+        # Condition : No Payment
+
+        # Create Invoice
         data = self._prepare_invoice()
         inv = self.obj_invoice.create(data)
         inv.signal_workflow('invoice_open')
@@ -144,3 +195,156 @@ class TestQueryReceivableAging(TransactionCase):
         self.assertEqual(query.aging3, 0.0)
         self.assertEqual(query.aging4, 0.0)
         self.assertEqual(query.aging5, 510.0)
+
+    def test_query_receivable_aging_2(self):
+        cr, uid = self.cr, self.uid
+        # Condition : Half Payment
+
+        # Create Invoice
+        data_invoice = self._prepare_invoice()
+        inv = self.obj_invoice.create(data_invoice)
+
+        inv.signal_workflow('invoice_open')
+
+        # Search Move
+        move_id = self.obj_move.search(
+            [('ref', '=', 'Customer Invoice - A')]
+        )
+        # Check Move
+        self.assertIsNotNone(move_id)
+
+        # Search Query
+
+        query_id = self.obj_query.search(
+            [('move_id', '=', move_id.id)]
+        )
+        # Check Query
+        self.assertEqual(query_id.move_id.name, move_id.name)
+
+        # Create Payment
+        data_bank_stmt = self._prepare_bank_statement()
+        bank_stmt_id = self.obj_bank_stmt.create(data_bank_stmt).id
+
+        # Check Payment
+        self.assertIsNotNone(bank_stmt_id)
+
+        # Create Payment Line
+        data_bank_stmt_line = self._prepare_bank_statement_line(bank_stmt_id)
+        bank_stmt_line_id = self.obj_bank_stmt_line.create(
+            cr, uid, data_bank_stmt_line)
+
+        # Check Payment Line
+        self.assertIsNotNone(bank_stmt_line_id)
+
+        # Create reconcile the payment with the invoice
+        for l in inv.move_id.line_id:
+            if l.account_id.id == self.account.id:
+                line_id = l
+                break
+
+        self.obj_bank_stmt_line.process_reconciliation(
+            cr, uid, bank_stmt_line_id, [{
+                'counterpart_move_line_id': line_id.id,
+                'credit': 150,
+                'debit': 0,
+                'name': line_id.name, }])
+
+        # Create Payment 2
+        data_bank_stmt_2 = self._prepare_bank_statement_2()
+        bank_stmt_id_2 = self.obj_bank_stmt.create(data_bank_stmt_2).id
+
+        # Check Payment 2
+        self.assertIsNotNone(bank_stmt_id_2)
+
+        # Create Payment Line 2
+        data_bank_stmt_line_2 = self._prepare_bank_statement_line_2(
+            bank_stmt_id_2)
+        bank_stmt_line_id_2 = self.obj_bank_stmt_line.create(
+            cr, uid, data_bank_stmt_line_2)
+
+        # Check Payment Line
+        self.assertIsNotNone(bank_stmt_line_id_2)
+
+        # Create reconcile the payment with the invoice
+        for l in inv.move_id.line_id:
+            if l.account_id.id == self.account.id:
+                line_id = l
+                break
+
+        self.obj_bank_stmt_line.process_reconciliation(
+            cr, uid, bank_stmt_line_id_2, [{
+                'counterpart_move_line_id': line_id.id,
+                'credit': 70,
+                'debit': 0,
+                'name': line_id.name, }])
+
+        # Check Query
+        self.assertEqual(query_id.move_id.name, move_id.name)
+
+        # Check Aging
+        # Condition : Period Length = 30
+
+        # Aging1 0-30
+        date_as_of_1 = date.fromordinal(
+            self.date_due.toordinal() + 1)
+
+        query = self.obj_query.with_context(
+            period_length=30, date_as_of=str(date_as_of_1)
+        ).browse(query_id.id)[0]
+        self.assertEqual(query.aging1, 360.0)
+        self.assertEqual(query.aging2, 0.0)
+        self.assertEqual(query.aging3, 0.0)
+        self.assertEqual(query.aging4, 0.0)
+        self.assertEqual(query.aging5, 0.0)
+
+        # Aging2 31-60
+        date_as_of_2 = date.fromordinal(
+            self.date_due.toordinal() + 31)
+
+        query = self.obj_query.with_context(
+            period_length=30, date_as_of=str(date_as_of_2)
+        ).browse(query_id.id)[0]
+        self.assertEqual(query.aging1, 0.0)
+        self.assertEqual(query.aging2, 360.0)
+        self.assertEqual(query.aging3, 0.0)
+        self.assertEqual(query.aging4, 0.0)
+        self.assertEqual(query.aging5, 0.0)
+
+        # Aging3 61-90
+        date_as_of_3 = date.fromordinal(
+            self.date_due.toordinal() + 61)
+
+        query = self.obj_query.with_context(
+            period_length=30, date_as_of=str(date_as_of_3)
+        ).browse(query_id.id)[0]
+        self.assertEqual(query.aging1, 0.0)
+        self.assertEqual(query.aging2, 0.0)
+        self.assertEqual(query.aging3, 290.0)
+        self.assertEqual(query.aging4, 0.0)
+        self.assertEqual(query.aging5, 0.0)
+
+        # Aging4 91-120
+        date_as_of_4 = date.fromordinal(
+            self.date_due.toordinal() + 91)
+
+        query = self.obj_query.with_context(
+            period_length=30, date_as_of=str(date_as_of_4)
+        ).browse(query_id.id)[0]
+        self.assertEqual(query.aging1, 0.0)
+        self.assertEqual(query.aging2, 0.0)
+        self.assertEqual(query.aging3, 0.0)
+        self.assertEqual(query.aging4, 290.0)
+        self.assertEqual(query.aging5, 0.0)
+
+        # Aging5 +120
+        date_as_of_5 = date.fromordinal(
+            self.date_due.toordinal() + 121)
+
+        query = self.obj_query.with_context(
+            period_length=30, date_as_of=str(date_as_of_5)
+        ).browse(query_id.id)[0]
+        self.assertEqual(query.aging1, 0.0)
+        self.assertEqual(query.aging2, 0.0)
+        self.assertEqual(query.aging3, 0.0)
+        self.assertEqual(query.aging4, 0.0)
+        self.assertEqual(query.aging5, 290.0)
