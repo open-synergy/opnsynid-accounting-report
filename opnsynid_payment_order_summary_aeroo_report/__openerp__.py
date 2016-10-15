88afae645f4c7366084cc1dc90322745ff954db9
Andhitia Rama <andhitia.r@gmail.com>
2016-10-15 17:44:37 +0700

    [ADD][8.0] opnsynid_payment_order_summary_aeroo_report

---
+++ opnsynid_payment_order_summary_aeroo_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Payment Order Summary",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "report_aeroo",
+        "account_payment",
+    ],
+    "data": [
+        "reports/payment_order_summary_reports.xml",
+        "wizards/print_payment_order_summary_views.xml",
+    ],
+}
