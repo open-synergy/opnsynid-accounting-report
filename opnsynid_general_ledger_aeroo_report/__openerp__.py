e9e7a0e6eea73924e24b28453e7cf48bfcc0fafe
Andhitia Rama <andhitia.r@gmail.com>
2018-05-22 12:25:12 +0700

    [8.0.1.0.1] opnsynid_general_ledger_aeroo_report
    * Remove unused security file

--- opnsynid_general_ledger_aeroo_report/__openerp__.py
+++ opnsynid_general_ledger_aeroo_report/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'General Ledger Report',
-    'version': '8.0.1.0.1',
+    'version': '8.0.1.0.2',
     'author': 'OpenSynergy Indonesia',
     'category': 'Accounting',
     'summary': 'Report General Ledger',
