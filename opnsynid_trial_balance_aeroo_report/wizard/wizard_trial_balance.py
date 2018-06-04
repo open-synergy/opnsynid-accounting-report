8f6b7e73b374ccb496ef2332aeb5088efa977969
Andhitia Rama <andhitia.r@gmail.com>
2018-06-04 17:23:14 +0700

    [TRAVIS] opnsynid_trial_balance_aeroo_report

--- opnsynid_trial_balance_aeroo_report/wizard/wizard_trial_balance.py
+++ opnsynid_trial_balance_aeroo_report/wizard/wizard_trial_balance.py
@@ -7,97 +7,97 @@ from openerp.osv import osv, fields
 from openerp.tools.translate import _
 
 
-class wizard_report_trial_balance(osv.osv_memory):
-    _name = 'account.wizard_report_trial_balance'
-    _description = 'Wizard Report Trial Balance'
+class WizardReportTrialBalance(osv.osv_memory):
+    _name = "account.wizard_report_trial_balance"
+    _description = "Wizard Report Trial Balance"
 
     def default_company_id(self, cr, uid, context=None):
-        obj_user = self.pool.get('res.users')
+        obj_user = self.pool.get("res.users")
 
         user = obj_user.browse(cr, uid, [uid])[0]
 
         return user.company_id and user.company_id.id or False
 
     def default_fiscalyear_id(self, cr, uid, context=None):
-        obj_fiscalyear = self.pool.get('account.fiscalyear')
+        obj_fiscalyear = self.pool.get("account.fiscalyear")
 
         fiscalyear_id = obj_fiscalyear.find(cr, uid)
 
         return fiscalyear_id or False
 
     def default_period_id(self, cr, uid, context=None):
-        obj_period = self.pool.get('account.period')
+        obj_period = self.pool.get("account.period")
 
         period_ids = obj_period.find(cr, uid)
 
         return period_ids and period_ids[0] or False
 
     def default_state(self, cr, uid, context=None):
-        return 'posted'
+        return "posted"
 
     def default_output(self, cr, uid, context=None):
-        return 'ods'
+        return "ods"
 
     _columns = {
-        'company_id': fields.many2one(
-            string='Company',
-            obj='res.company',
+        "company_id": fields.many2one(
+            string="Company",
+            obj="res.company",
             required=True),
-        'fiscalyear_id': fields.many2one(
-            string='Fiscal Year',
-            obj='account.fiscalyear',
+        "fiscalyear_id": fields.many2one(
+            string="Fiscal Year",
+            obj="account.fiscalyear",
             required=True),
-        'period_id': fields.many2one(
-            string='Period',
-            obj='account.period',
+        "period_id": fields.many2one(
+            string="Period",
+            obj="account.period",
             required=True),
-        'output_format': fields.selection(
-            string='Output Format',
+        "output_format": fields.selection(
+            string="Output Format",
             required=True,
             selection=[
-                ('pdf', 'PDF'),
-                ('xls', 'XLS'),
-                ('ods', 'ODS')
+                ("pdf", "PDF"),
+                ("xls", "XLS"),
+                ("ods", "ODS")
             ]),
-        'state': fields.selection(
-            string='State',
+        "state": fields.selection(
+            string="State",
             selection=[
-                ('all', 'All'),
-                ('draft', 'Draft'),
-                ('posted', 'Posted')
+                ("all", "All"),
+                ("draft", "Draft"),
+                ("posted", "Posted")
             ],
             required=True),
     }
 
     _defaults = {
-        'company_id': default_company_id,
-        'fiscalyear_id': default_fiscalyear_id,
-        'period_id': default_period_id,
-        'output_format': default_output,
-        'state': default_state
+        "company_id": default_company_id,
+        "fiscalyear_id": default_fiscalyear_id,
+        "period_id": default_period_id,
+        "output_format": default_output,
+        "state": default_state
     }
 
     def button_print_report(self, cr, uid, ids, data, context=None):
         datas = {}
-        output_format = ''
+        output_format = ""
 
         if context is None:
             context = {}
 
-        datas['form'] = self.read(cr, uid, ids)[0]
+        datas["form"] = self.read(cr, uid, ids)[0]
 
-        if datas['form']['output_format'] == 'xls':
-            output_format = 'report_trial_balance_xls'
-        elif datas['form']['output_format'] == 'ods':
-            output_format = 'report_trial_balance_ods'
-        elif datas['form']['output_format'] == 'pdf':
-            output_format = 'report_trial_balance_pdf'
+        if datas["form"]["output_format"] == "xls":
+            output_format = "report_trial_balance_xls"
+        elif datas["form"]["output_format"] == "ods":
+            output_format = "report_trial_balance_ods"
+        elif datas["form"]["output_format"] == "pdf":
+            output_format = "report_trial_balance_pdf"
         else:
-            err = 'Output Format cannot be empty'
-            raise osv.except_osv(_('Warning'), _(err))
+            err = "Output Format cannot be empty"
+            raise osv.except_osv(_("Warning"), _(err))
 
         return {
-            'type': 'ir.actions.report.xml',
-            'report_name': output_format,
-            'datas': datas,
+            "type": "ir.actions.report.xml",
+            "report_name": output_format,
+            "datas": datas,
         }
