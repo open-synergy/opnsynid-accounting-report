c481121d8e5f3b43cd03a73f95a4f131b535b4a4
Andhitia Rama <andhitia.r@gmail.com>
2018-06-04 17:12:01 +0700

    [TRAVIS] opnsynid_receivable_aging_aeroo_report

--- opnsynid_receivable_aging_aeroo_report/report/report_receivable_aging.py
+++ opnsynid_receivable_aging_aeroo_report/report/report_receivable_aging.py
@@ -14,36 +14,36 @@ class Parser(report_sxw.rml_parse):
         self.journal_list = []
         self.total_amount = 0.0
         self.localcontext.update({
-            'time': time,
-            'get_company': self.get_company,
-            'get_date': self.get_date,
-            'get_date_as_of': self.get_date_as_of,
-            'get_period_length': self.get_period_length,
-            'get_journals': self.get_journals,
+            "time": time,
+            "get_company": self.get_company,
+            "get_date": self.get_date,
+            "get_date_as_of": self.get_date_as_of,
+            "get_period_length": self.get_period_length,
+            "get_journals": self.get_journals,
         })
 
     def get_company(self):
-        data = self.localcontext['data']['form']
-        company_name = data['company_id'] and data['company_id'][1] or False
+        data = self.localcontext["data"]["form"]
+        company_name = data["company_id"] and data["company_id"][1] or False
 
         return company_name
 
     def get_date_from(self):
-        data = self.localcontext['data']['form']
-        date_from = data['date_from'] or False
+        data = self.localcontext["data"]["form"]
+        date_from = data["date_from"] or False
         if date_from:
             conv_date_from = datetime.strptime(
-                date_from, '%Y-%m-%d').strftime('%d/%m/%Y')
+                date_from, "%Y-%m-%d").strftime("%d/%m/%Y")
             return conv_date_from
         else:
             return False
 
     def get_date_to(self):
-        data = self.localcontext['data']['form']
-        date_to = data['date_to'] or False
+        data = self.localcontext["data"]["form"]
+        date_to = data["date_to"] or False
         if date_to:
             conv_date_to = datetime.strptime(
-                date_to, '%Y-%m-%d').strftime('%d/%m/%Y')
+                date_to, "%Y-%m-%d").strftime("%d/%m/%Y")
             return conv_date_to
         else:
             return False
@@ -53,33 +53,33 @@ class Parser(report_sxw.rml_parse):
         date_to = self.get_date_to()
 
         if date_from and date_to:
-            return date_from + ' to ' + date_to
+            return date_from + " to " + date_to
         elif date_from and not date_to:
-            return 'from ' + date_from
+            return "from " + date_from
         elif not date_from and date_to:
-            return 'to ' + date_to
+            return "to " + date_to
         else:
             return False
 
     def get_date_as_of(self):
-        data = self.localcontext['data']['form']
-        date_as_of = data['date_as_of'] or False
+        data = self.localcontext["data"]["form"]
+        date_as_of = data["date_as_of"] or False
         conv_date_as_of = datetime.strptime(
-            date_as_of, '%Y-%m-%d').strftime('%d/%m/%Y')
+            date_as_of, "%Y-%m-%d").strftime("%d/%m/%Y")
 
         return conv_date_as_of
 
     def get_period_length(self):
-        data = self.localcontext['data']['form']
-        period_length = data['period_length'] or False
+        data = self.localcontext["data"]["form"]
+        period_length = data["period_length"] or False
 
         return period_length
 
     def get_journals(self):
-        obj_journal = self.pool.get('account.journal')
-        data_form = self.localcontext['data']['form']
-        journal_ids = data_form['journal_ids']
-        period_length = data_form['period_length']
+        obj_journal = self.pool.get("account.journal")
+        data_form = self.localcontext["data"]["form"]
+        journal_ids = data_form["journal_ids"]
+        period_length = data_form["period_length"]
 
         if journal_ids:
             for journal in obj_journal.browse(self.cr, self.uid, journal_ids):
@@ -89,28 +89,28 @@ class Parser(report_sxw.rml_parse):
                 for interval in range(1, 6):
                     label1 = str(period_length * (interval - 1))
                     label2 = str(period_length * (interval))
-                    aging_label['aging%s' % interval] = '%s - %s' % (
+                    aging_label["aging%s" % interval] = "%s - %s" % (
                         label1, label2)
 
                     if interval == 5:
-                        aging_label['aging5'] = '+%s' % (
+                        aging_label["aging5"] = "+%s" % (
                             str(period_length * (4)))
 
                 result = {
-                    'id': journal.id,
-                    'name': journal.name,
-                    'data': data['data'],
-                    'aging1': data['aging1'],
-                    'aging2': data['aging2'],
-                    'aging3': data['aging3'],
-                    'aging4': data['aging4'],
-                    'aging5': data['aging5'],
-                    'current': data['current'],
-                    'aging_label1': aging_label['aging1'],
-                    'aging_label2': aging_label['aging2'],
-                    'aging_label3': aging_label['aging3'],
-                    'aging_label4': aging_label['aging4'],
-                    'aging_label5': aging_label['aging5'],
+                    "id": journal.id,
+                    "name": journal.name,
+                    "data": data["data"],
+                    "aging1": data["aging1"],
+                    "aging2": data["aging2"],
+                    "aging3": data["aging3"],
+                    "aging4": data["aging4"],
+                    "aging5": data["aging5"],
+                    "current": data["current"],
+                    "aging_label1": aging_label["aging1"],
+                    "aging_label2": aging_label["aging2"],
+                    "aging_label3": aging_label["aging3"],
+                    "aging_label4": aging_label["aging4"],
+                    "aging_label5": aging_label["aging5"],
                 }
 
                 self.journal_list.append(result)
@@ -119,31 +119,31 @@ class Parser(report_sxw.rml_parse):
 
     def get_data(self, journal_id):
         res = {}
-        obj_line = self.pool.get('account.query_receivable_aging')
+        obj_line = self.pool.get("account.query_receivable_aging")
 
-        company_id = self.localcontext['data']['form']['company_id'][0]
-        date_from = self.localcontext['data']['form']['date_from']
-        date_to = self.localcontext['data']['form']['date_to']
-        date_as_of = self.localcontext['data']['form']['date_as_of']
-        fiscalyear_id = self.localcontext['data']['form']['fiscalyear_id'][0]
-        period_length = self.localcontext['data']['form']['period_length']
+        company_id = self.localcontext["data"]["form"]["company_id"][0]
+        date_from = self.localcontext["data"]["form"]["date_from"]
+        date_to = self.localcontext["data"]["form"]["date_to"]
+        date_as_of = self.localcontext["data"]["form"]["date_as_of"]
+        fiscalyear_id = self.localcontext["data"]["form"]["fiscalyear_id"][0]
+        period_length = self.localcontext["data"]["form"]["period_length"]
 
         criteria = [
-            ('company_id.id', '=', company_id),
-            ('move_id.period_id.fiscalyear_id', '=', fiscalyear_id),
-            ('journal_id.id', '=', journal_id),
-            ('state', '=', 'posted')
+            ("company_id.id", "=", company_id),
+            ("move_id.period_id.fiscalyear_id", "=", fiscalyear_id),
+            ("journal_id.id", "=", journal_id),
+            ("state", "=", "posted")
         ]
 
         if date_from and not date_to:
-            criteria.append(('date', '>=', date_from))
+            criteria.append(("date", ">=", date_from))
         if not date_from and date_to:
-            criteria.append(('date', '<=', date_to))
+            criteria.append(("date", "<=", date_to))
         if date_from and date_to:
-            criteria.append(('date', '>=', date_from))
-            criteria.append(('date', '<=', date_to))
+            criteria.append(("date", ">=", date_from))
+            criteria.append(("date", "<=", date_to))
 
-        context = {'date_as_of': date_as_of, 'period_length': period_length}
+        context = {"date_as_of": date_as_of, "period_length": period_length}
 
         data_list = []
         result = {}
@@ -158,43 +158,43 @@ class Parser(report_sxw.rml_parse):
                     self.cr, self.uid, line_ids, context=context):
                 if line.amount_residual > 0:
                     conv_date = datetime.strptime(
-                        line.date, '%Y-%m-%d').strftime('%d-%m-%Y')
+                        line.date, "%Y-%m-%d").strftime("%d-%m-%Y")
                     conv_date_due = datetime.strptime(
-                        line.date_due, '%Y-%m-%d').strftime('%d-%m-%Y')
-                    if line.direction == 'past':
+                        line.date_due, "%Y-%m-%d").strftime("%d-%m-%Y")
+                    if line.direction == "past":
                         res = {
-                            'no': no,
-                            'date': conv_date,
-                            'date_due': conv_date_due,
-                            'transaction': line.move_id.name,
-                            'description': line.name,
-                            'partner': line.partner_id.name,
-                            'current': 0.0,
-                            'aging1': line.aging1,
-                            'aging2': line.aging2,
-                            'aging3': line.aging3,
-                            'aging4': line.aging4,
-                            'aging5': line.aging5,
+                            "no": no,
+                            "date": conv_date,
+                            "date_due": conv_date_due,
+                            "transaction": line.move_id.name,
+                            "description": line.name,
+                            "partner": line.partner_id.name,
+                            "current": 0.0,
+                            "aging1": line.aging1,
+                            "aging2": line.aging2,
+                            "aging3": line.aging3,
+                            "aging4": line.aging4,
+                            "aging5": line.aging5,
                         }
                         aging1 += line.aging1
                         aging2 += line.aging2
                         aging3 += line.aging3
                         aging4 += line.aging4
                         aging5 += line.aging5
-                    if line.direction == 'future':
+                    if line.direction == "future":
                         res = {
-                            'no': no,
-                            'date': conv_date,
-                            'date_due': conv_date_due,
-                            'transaction': line.move_id.name,
-                            'description': line.name,
-                            'partner': line.partner_id.name,
-                            'current': line.amount_residual,
-                            'aging1': 0.0,
-                            'aging2': 0.0,
-                            'aging3': 0.0,
-                            'aging4': 0.0,
-                            'aging5': 0.0,
+                            "no": no,
+                            "date": conv_date,
+                            "date_due": conv_date_due,
+                            "transaction": line.move_id.name,
+                            "description": line.name,
+                            "partner": line.partner_id.name,
+                            "current": line.amount_residual,
+                            "aging1": 0.0,
+                            "aging2": 0.0,
+                            "aging3": 0.0,
+                            "aging4": 0.0,
+                            "aging5": 0.0,
                         }
                         current += line.amount_residual
 
@@ -202,12 +202,12 @@ class Parser(report_sxw.rml_parse):
                     no += 1
 
         result = {
-            'data': data_list,
-            'aging1': aging1,
-            'aging2': aging2,
-            'aging3': aging3,
-            'aging4': aging4,
-            'aging5': aging5,
-            'current': current
+            "data": data_list,
+            "aging1": aging1,
+            "aging2": aging2,
+            "aging3": aging3,
+            "aging4": aging4,
+            "aging5": aging5,
+            "current": current
         }
         return result
--- opnsynid_receivable_aging_aeroo_report/wizard/wizard_receivable_aging.py
+++ opnsynid_receivable_aging_aeroo_report/wizard/wizard_receivable_aging.py
@@ -9,12 +9,12 @@ from datetime import datetime
 
 
 class WizardReportReceivableAging(models.TransientModel):
-    _name = 'account.wizard_report_receivable_aging'
-    _description = 'Wizard Report Receivable Aging'
+    _name = "account.wizard_report_receivable_aging"
+    _description = "Wizard Report Receivable Aging"
 
     @api.model
     def _default_fiscalyear_id(self):
-        obj_fiscalyear = self.env['account.fiscalyear']
+        obj_fiscalyear = self.env["account.fiscalyear"]
 
         fiscalyear_id = obj_fiscalyear.find()
 
@@ -27,68 +27,67 @@ class WizardReportReceivableAging(models.TransientModel):
     @api.model
     def _default_journal(self):
         company_id = self._context.get(
-            'company_id', self.env.user.company_id.id)
+            "company_id", self.env.user.company_id.id)
         domain = [
-            ('type', '=', 'sale'),
-            ('company_id', '=', company_id),
+            ("type", "=", "sale"),
+            ("company_id", "=", company_id),
         ]
-        return self.env['account.journal'].search(domain, limit=1)
+        return self.env["account.journal"].search(domain, limit=1)
 
     company_id = fields.Many2one(
-        string='Company',
-        comodel_name='res.company',
+        string="Company",
+        comodel_name="res.company",
         required=True,
         default=lambda self: self.env.user.company_id.id,
-        )
+    )
 
     fiscalyear_id = fields.Many2one(
-        string='Fiscal Year',
-        comodel_name='account.fiscalyear',
+        string="Fiscal Year",
+        comodel_name="account.fiscalyear",
         required=True,
         default=_default_fiscalyear_id,
-        )
+    )
 
     journal_ids = fields.Many2many(
-        string='Journals',
-        comodel_name='account.journal',
-        relation='wizard_receivable_aging_rel',
-        column1='wizard_id',
-        column2='journal_id',
-        domain="[('type','=','sale'), ('company_id', '=', [company_id])]",
+        string="Journals",
+        comodel_name="account.journal",
+        relation="wizard_receivable_aging_rel",
+        column1="wizard_id",
+        column2="journal_id",
         default=_default_journal,
     )
 
     date_from = fields.Date(
-        string='Date From',
+        string="Date From",
         required=False,
-        )
+    )
 
     date_to = fields.Date(
-        string='Date To',
+        string="Date To",
         required=False,
-        )
+    )
 
     date_as_of = fields.Date(
-        string='Date As Of',
+        string="Date As Of",
         required=True,
         default=datetime.now(),
-        )
+    )
 
     period_length = fields.Integer(
-        string='Period Legth(days)',
+        string="Period Legth(days)",
         required=True,
         default=30,
-        )
+    )
 
     output_format = fields.Selection(
-        string='Output Format',
+        string="Output Format",
         required=True,
         selection=[
-            ('xls', 'XLS'),
-            ('ods', 'ODS')
+            ("xls", "XLS"),
+            ("ods", "ODS")
         ],
-        default='ods',
-        )
+        default="ods",
+    )
 
     @api.multi
     def button_print_report(self):
@@ -99,25 +98,25 @@ class WizardReportReceivableAging(models.TransientModel):
             context = {}
 
         datas = {}
-        output_format = ''
+        output_format = ""
 
-        datas['form'] = self.read()[0]
+        datas["form"] = self.read()[0]
 
-        date_from = datas['form']['date_from']
-        date_to = datas['form']['date_to']
+        date_from = datas["form"]["date_from"]
+        date_to = datas["form"]["date_to"]
 
         if date_from and date_to:
             if date_from > date_to:
-                err = 'Date From cannot be greater than Date To'
-                raise except_orm(_('Warning'), _(err))
+                err = "Date From cannot be greater than Date To"
+                raise except_orm(_("Warning"), _(err))
 
-        if datas['form']['output_format'] == 'xls':
-            output_format = 'report_receivable_aging_xls'
-        elif datas['form']['output_format'] == 'ods':
-            output_format = 'report_receivable_aging_ods'
+        if datas["form"]["output_format"] == "xls":
+            output_format = "report_receivable_aging_xls"
+        elif datas["form"]["output_format"] == "ods":
+            output_format = "report_receivable_aging_ods"
 
         return {
-            'type': 'ir.actions.report.xml',
-            'report_name': output_format,
-            'datas': datas,
+            "type": "ir.actions.report.xml",
+            "report_name": output_format,
+            "datas": datas,
         }
