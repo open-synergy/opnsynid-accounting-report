2e54a66a75a67faa7ad9164a8dd886d92775ea8e
Andhitia Rama <andhitia.r@gmail.com>
2018-06-04 16:27:26 +0700

    [TRAVIS] opnsynid_general_ledger_aeroo_report

--- opnsynid_general_ledger_aeroo_report/__openerp__.py
+++ opnsynid_general_ledger_aeroo_report/__openerp__.py
@@ -3,23 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'General Ledger Report',
-    'version': '8.0.1.0.2',
-    'author': 'OpenSynergy Indonesia',
-    'category': 'Accounting',
-    'summary': 'Report General Ledger',
-    'website': 'https://opensynergy-indonesia.com',
-    'depends': [
-        'account_accountant',
-        'report_aeroo'
+    "name": "General Ledger Report",
+    "version": "8.0.1.0.2",
+    "author": "OpenSynergy Indonesia",
+    "category": "Accounting",
+    "summary": "Report General Ledger",
+    "website": "https://opensynergy-indonesia.com",
+    "depends": [
+        "account_accountant",
+        "report_aeroo"
     ],
-    'data': [
-        'report/report_general_ledger.xml',
-        'wizard/wizard_general_ledger.xml',
-        'menu_Accounting.xml'
+    "data": [
+        "report/report_general_ledger.xml",
+        "wizard/wizard_general_ledger.xml",
+        "menu_Accounting.xml"
     ],
-    'installable': True,
-    'auto_install': False,
-    'application': True,
-    'license': 'AGPL-3'
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+    "license": "AGPL-3"
 }
