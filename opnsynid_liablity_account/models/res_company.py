4ed2417a83a40bc31f36ec3e471e54e5fb2b1ba7
Andhitia Rama <andhitia.r@gmail.com>
2018-06-04 17:19:13 +0700

    [TRAVIS] opnsynid_liability_account

--- opnsynid_liablity_account/models/res_company.py
+++ opnsynid_liablity_account/models/res_company.py
@@ -5,15 +5,15 @@
 from openerp import models, fields
 
 
-class res_company(models.Model):
+class ResCompany(models.Model):
     """override company to add liablity account"""
-    _inherit = 'res.company'
-    _name = 'res.company'
+    _inherit = "res.company"
+    _name = "res.company"
 
     liability_ids = fields.Many2many(
-        string='Liabilities',
-        comodel_name='account.account',
-        relation='rel_company_2_liablity_acc',
-        column1='company_id',
-        column2='account_id',
+        string="Liabilities",
+        comodel_name="account.account",
+        relation="rel_company_2_liablity_acc",
+        column1="company_id",
+        column2="account_id",
         )
