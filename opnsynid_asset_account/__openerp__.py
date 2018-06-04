fe2a75ac8b7ba15ef40ca1924aaf5378cc628aea
Andhitia Rama <andhitia.r@gmail.com>
2018-06-04 16:55:16 +0700

    [TRAVIS] opnsynid_asset_account

--- opnsynid_asset_account/__openerp__.py
+++ opnsynid_asset_account/__openerp__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Asset Accounts Configurator Helper',
-    'version': '8.0.1.0.0',
-    'author': 'OpenSynergy Indonesia',
-    'category': 'Accounting',
-    'summary': 'Asset view helper',
-    'website': 'https://opensynergy-indonesia.com',
-    'depends': [
-        'opnsynid_accounting_report_configuration_page',
+    "name": "Asset Accounts Configurator Helper",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "category": "Accounting",
+    "summary": "Asset view helper",
+    "website": "https://opensynergy-indonesia.com",
+    "depends": [
+        "opnsynid_accounting_report_configuration_page",
         ],
-    'data': [
-        'views/res_company_view.xml',
+    "data": [
+        "views/res_company_view.xml",
         ],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
-    'license': 'AGPL-3',
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "license": "AGPL-3",
 }
--- opnsynid_asset_account/models/res_company.py
+++ opnsynid_asset_account/models/res_company.py
@@ -5,15 +5,15 @@
 from openerp import models, fields
 
 
-class res_company(models.Model):
+class ResCompany(models.Model):
     """override company to add asset account"""
-    _inherit = 'res.company'
-    _name = 'res.company'
+    _inherit = "res.company"
+    _name = "res.company"
 
     asset_ids = fields.Many2many(
-        string='Assets',
-        comodel_name='account.account',
-        relation='rel_company_2_asset_acc',
-        column1='company_id',
-        column2='account_id',
+        string="Assets",
+        comodel_name="account.account",
+        relation="rel_company_2_asset_acc",
+        column1="company_id",
+        column2="account_id",
         )
