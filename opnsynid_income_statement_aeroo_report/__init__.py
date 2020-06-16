ba15acd61ee173905af205ebcb66287588fb89b8
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-16 14:19:21 +0700

    [8.0.1.0.2][opnsynid_income_statement_aeroo_report]

--- opnsynid_income_statement_aeroo_report/__init__.py
+++ opnsynid_income_statement_aeroo_report/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2015 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import wizards
-from . import reports
+# Copyright 2015 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import(
+    wizards,
+    reports,
+)
--- opnsynid_income_statement_aeroo_report/__openerp__.py
+++ opnsynid_income_statement_aeroo_report/__openerp__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-# © 2015 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2015 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Income statement report",
-    "version": "8.0.1.0.1",
-    "author": "OpenSynergy Indonesia",
-    "category": "Accounting",
+    "version": "8.0.1.0.2",
     "summary": "Income statement report",
-    "website": "https://opensynergy-indonesia.com",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "depends": [
         "opnsynid_income_statement_account",
         "report_aeroo",
@@ -16,6 +17,9 @@
         "wizards/wizard_income_statement.xml",
         "reports/report_income_statement_ods.xml",
         "reports/report_income_statement_xls.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
     "installable": True,
     "auto_install": False,
